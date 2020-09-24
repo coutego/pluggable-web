b6637582a5335cb6ee77710b97d2015a330c3fef
Pedro Abelleira Seco <pedroabelleira@gmail.com>
2020-09-25 01:16:04 +0200

    Small code cleanup

--- src/pluggable_web/sample_app.cljs
+++ src/pluggable_web/sample_app.cljs
@@ -1,11 +1,12 @@
 (ns pluggable-web.sample-app
+  "Sample app for pluggable web (under construction)"
   (:require [pluggable-web.core :as pw]
             [pluggable-web.spa.core :as spa]
             [pluggable-web.template.core :as template]
             [pluggable-web.routing.core :as routing]
             [pluggable-web.notifications.core :as notifications]))
 
-(defn ^:dev/after-load init4 []
+(defn ^:dev/after-load init []
   (pw/push-plugins! [spa/plugin
                      template/plugin
                      routing/plugin
