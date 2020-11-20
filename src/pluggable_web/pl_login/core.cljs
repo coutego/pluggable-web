3a0994e478106b97ec6e1e759cd3194252709a55
Pedro Abelleira Seco <pedroabelleira@gmail.com>
2020-11-21 00:17:33 +0100

    Added missing file

--- src/pluggable_web/pl_login/core.cljs
+++ src/pluggable_web/pl_login/core.cljs
@@ -2,10 +2,8 @@
   (:require [pluggable-web.pl-security.core :as sec]
             [pluggable-web.pl-template.core :as template]
             [pluggable-web.pl-routing.core :as routing]
-            [pluggable-web.pl-login.pages.main :as page]))
-
-;; (defprotocol IUserInfo
-;;   (-user-data [_])) ; :name :surname :initials 
+            [pluggable-web.pl-login.pages.main :as page]
+            [reagent.core :as r]))
 
 ;; (defrecord User [name surname type roles])
 
@@ -15,30 +13,56 @@
 ;;    (->User "John" "Doe" "Admin" [:user :admin])
 ;;    (->User "Ada" "Lovelace" "Developer" [:dev :user])])
 
+(defprotocol IUserInfo
+  (-user-data [_])) ; :name :surname :initials 
+
+(defprotocol -IUserManager
+  (-set-current-user! [_ user-data roles])) 
+
+(defn create-srv-user []
+  (let [current-user (r/atom nil)]
+    (reify
+      IUserInfo
+      (-user-data [_]
+        (:user-data @current-user))
+      -IUserManager
+      (-set-current-user! [_ user-data roles]
+        (swap! @current-user #(do (assoc % :user-data user-data)
+                                  (assoc % :roles roles))))
+      sec/IUserRoles
+      (-roles [_] (:roles @current-user)))))
+
 (defn ui-logged-user [initials-fn on-click]
   [:div.ui.text.container
    {:on-click on-click}
    [:div.right.floated.item
-    (if initials-fn
-      [:a.ui.blue.label (initials-fn)]
+    (if-let [initials (and initials-fn (initials-fn))]
+      [:a.ui.blue.label initials]
       [:a {:href :#} [:i.ui.user.icon] "Log in"])]])
 
 (defn ui-logging-page []
   [:h1 "Login"])
 
-(defn on-go-login-page [router]
+(defn fn-on-go-login-page [router]
   #(routing/goto router ::login-page))
 
-(defn fn-initials [] "PAS")
+(defn fn-initials [srv-user]
+  (fn [x] "FOO"))
+
+  ;; #(or (:initials (-user-data srv-user)) "Login"))
 
 (def plugin
-  {:id ::login
+  {:id                     ::login
    ::template/topbar-right ::ui-logged-user
-   ::routing/routes [["/login"
-                      {:name ::login-page
-                       :view ::login-page}]]
+   ::routing/routes        [["/login"
+                             {:name ::login-page
+                              :view ::login-page}]]
    :beans
-   {::ui-logged-user [ui-logged-user #'fn-initials ::on-go-login-page]
-    ::on-go-login-page [on-go-login-page ::routing/router]
-    ::login-page page/ui-login-page}})
+   {::ui-logged-user   [ui-logged-user #'fn-initials ::on-go-login-page]
+    ::fn-initials      [fn-initials ::srv-user]
+    ::on-go-login-page [fn-on-go-login-page ::routing/router]
+
+    ::login-page page/ui-login-page}
+   :reusable-beans
+   {::srv-user [vector create-srv-user]}})
 
